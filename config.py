c7488949dc0f1ed6cc4979ffebe093e21b67c750
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-19 15:27:04 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+APPLICATION_ID = os.environ["APPLICATION_ID"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
+PUBLIC_KEY = os.environ["PUBLIC_KEY"]
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 OWNER_IDS = [546682137240403984]
 
 ADDRESS = os.environ["DB_ADDRESS"]
