53419013e667f0a6564bb689c54e74a4e6e5840f
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-19 14:50:33 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+OWNER_IDS = [546682137240403984]
+
+ADDRESS = os.environ["DB_ADDRESS"]
+PORT = 3306
+USER_NAME = os.environ["DB_USERNAME"]
+PASSWORD = os.environ["DB_PASSWD"]
+DB_NAME = os.environ["DB_NAME"]
