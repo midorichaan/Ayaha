3fa700a9e418237cabc620af2f4d4382468a62f8
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-22 01:06:40 +0900

    Update mido_bot.py

--- cogs/mido_bot.py
+++ cogs/mido_bot.py
@@ -15,27 +15,29 @@ class mido_bot(commands.Cog):
     async def ping(self, ctx):
         msg_time = time.perf_counter()
         
-        lang = await self.bot.langutil.get_lang(ctx.author.id, key="ping-pinging")
-        m = await utils.reply_or_send(ctx, content=f"> {lang}")
-        
-        lang = await self.bot.langutil.get_lang(ctx.author.id, key="ping-pong")
+        lang = await self.bot.langutil.get_user_lang(ctx.author.id)
+        d = await self.bot.langutil.get_lang(lang)
+        m = await utils.reply_or_send(ctx, content=f"> {d['ping-pinging']}")
+
         ws = round(self.bot.latency * 1000, 2)
         ping = round(time.perf_counter() - msg_time, 3) * 1000
-        await m.edit(content=f"> {lang} \nPing: {ping} \nWebSocket: {ws}")
+        await m.edit(content=f"> {d['ping-pong']} \nPing: {ping} \nWebSocket: {ws}")
     
     #about
     @commands.command(aliases=["info", "bot", "ayaha"])
     async def about(self, ctx):
-        lang = await self.bot.langutil.get_lang(ctx.author.id, key="loading")
-        m = await utils.reply_or_send(ctx, content=f"> {lang}")
-        e = discord.Embed(title=await self.bot.langutil.get_lang(ctx.author.id, key="about-ayaha"), 
-                          description=await self.bot.langutil.get_lang(ctx.author.id, key="about-ayaha-description"),
+        lang = await self.bot.langutil.get_user_lang(ctx.author.id)
+        d = await self.bot.langutil.get_lang(lang)
+
+        m = await utils.reply_or_send(ctx, content=f"> {d['loading']}")
+        e = discord.Embed(title=d["about-ayaha"]), 
+                          description=d["about-ayaha-description"]),
                           color=self.bot.color,
                           timestamp=ctx.message.created_at
                          )
-        e.add_field(name=await self.bot.langutil.get_lang(ctx.author.id, key="guilds"), value=str(len(self.bot.guilds)))
-        e.add_field(name=await self.bot.langutil.get_lang(ctx.author.id, key="users"), value=str(len(self.bot.users)))
-        e.add_field(name=await self.bot.langutil.get_lang(ctx.author.id, key="invites"), 
+        e.add_field(name=d["guilds"]), value=str(len(self.bot.guilds)))
+        e.add_field(name=d["users"]), value=str(len(self.bot.users)))
+        e.add_field(name=d["invites"]), 
                     value="https://discord.com/oauth2/authorize?client_id=911139204531122257&scope=bot", 
                     inline=False
                    )
