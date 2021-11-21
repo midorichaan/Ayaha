ec88365f3343e274c7e52fb8f1f7ab0b48c5bcc8
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-11-22 03:58:44 +0900

    Update mido_guild_settings.py

--- cogs/mido_guild_settings.py
+++ cogs/mido_guild_settings.py
@@ -10,18 +10,34 @@ class mido_guild_settings(commands.Cog):
         self.bot = bot
     
     #build_gs_embed
-    async def build_gs_embed(self, ctx, type: int, *, lang: str=None):
+    async def build_gs_embed(self, ctx, type: int, db, *, lang: str=None):
         if not lang:
             lang = await self.bot.langutil.get_user_lang(ctx.author.id)
         d = await self.bot.langutil.get_lang(lang)
-        if type == 0:
+        if type == 1:
+            e = discord.Embed(title=d["guildsettings-prefix"], 
+                              description=d["guildsettings-set-prefix"],
+                              color=self.bot.color,
+                              timestamp=ctx.message.created_at
+                             )
+            return e
+        elif type == 2:
+            e = discord.Embed(title=d["guildsettings-baseprefix"], 
+                              description=d["guildsettings-toggle-baseprefix"],
+                              color=self.bot.color,
+                              timestamp=ctx.message.created_at
+                             )
+            return e
+        else:
             e = discord.Embed(title=d["guildsettings"], 
                               description="",
                               color=self.bot.color,
                               timestamp=ctx.message.created_at
                              )
-        
+            
             settings = [
+                "📝: {} ({})".format(d["guild-prefix"], db["prefix"] or d["guildsettings-no-prefix"]),
+                "📚: {} ({})".format(d["toggle-base-prefix"], d["guildsettings-true"]) if db["disable_base_prefix"] else d["guildsettings-false"],
                 "❌: {}".format(d["cancel"])
             ]
         
@@ -45,8 +61,11 @@ class mido_guild_settings(commands.Cog):
         d = await self.bot.langutil.get_lang(lang)
         
         m = await utils.reply_or_send(ctx, content=f"> {d['loading']}")
-        await m.edit(content=None, embed=await self.build_gs_embed(ctx, 0, lang=lang))
+        await m.edit(content=None, embed=await self.build_gs_embed(ctx, 0, db, lang=lang))
+        await m.add_reaction("📝")
+        await m.add_reaction("📚")
         await m.add_reaction("❌")
+        gs = await self.bot.db.fetchone("SELECT * FROM guilds WHERE guild_id=%s", (ctx.guild.id,))
         
         while True:
             try:
@@ -59,7 +78,33 @@ class mido_guild_settings(commands.Cog):
                 if ctx.channel.permissions_for(ctx.me).manage_messages:
                     await m.remove_reaction(str(r), ctx.author)
                 
-                if r.emoji == "❌":
+                if r.emoji == "📝":
+                    await self.clear_reactions(ctx, m)
+                    await m.edit(embed=await self.build_gs_embed(ctx, 1, db))
+                
+                    msg = await self.bot.wait_for("message", check=lambda m: m.author.id == ctx.author.id and m.channel.id == ctx.channel.id)
+                    await msg.delete()
+                    
+                    await self.bot.db.execute("UPDATE guilds SET prefix=%s WHERE guild_id=%s", (msg.content, ctx.guild.id))
+                    await m.edit(content=None, embed=await self.build_gs_embed(ctx, 0, db))
+                    await m.add_reaction("📝")
+                    await m.add_reaction("📚")
+                    await m.add_reaction("❌")
+                elif r.emoji == "📚":
+                    await self.clear_reactions(ctx, m)
+                    await m.edit(embed=await self.build_gs_embed(ctx, 2, db))
+                    
+                    if gs["disable_base_prefix"]:
+                        await self.bot.db.execute("UPDATE guilds SET disable_base_prefix=%s WHERE guild_id=%s", (0, ctx.guild.id,))
+                    else:
+                        await self.bot.db.execute("UPDATE guilds SET disable_base_prefix=%s WHERE guild_id=%s", (1, ctx.guild.id,))
+                    await asyncio.sleep(3)
+                    
+                    await m.edit(content=None, embed=await self.build_gs_embed(ctx, 0, db))
+                    await m.add_reaction("📝")
+                    await m.add_reaction("📚")
+                    await m.add_reaction("❌")
+                elif r.emoji == "❌":
                     await self.clear_reactions(ctx, m)
                     break
 
