fd2683bc8754b2a6a390805725ebc1f506833356
midorichan <55083684+midorichaan@users.noreply.github.com>
2021-12-16 23:31:19 +0900

    Update mido_admins.py

--- cogs/mido_admins.py
+++ cogs/mido_admins.py
@@ -47,11 +47,30 @@ class mido_admins(commands.Cog):
             return await m.edit(content=f"> {d['args-required']}")
         
         try:
-            await self.bot.db.ban_user(target.id, reason=reason)
+            await self.bot.db.ban_user(target.id, ctx.author.id, reason=reason)
         except Exception as exc:
             return await m.edit(content=f"> {d['error']} \n```py\n{exc}\n```")
         else:
             await m.edit(content=f"> {d['prohibit-banned'].replace('{TARGET}', {target})}")
+    
+    #unprohibit
+    @commands.is_owner()
+    @commands.command(usage="unprohibit <user/member>")
+    async def prohibit(self, ctx, target: utils.FetchUserConverter=None):
+        lang = await self.bot.langutil.get_user_lang(ctx.author.id)
+        d = await self.bot.langutil.get_lang(lang)
+        
+        m = await utils.reply_or_send(ctx, content=f"> {d['loading']}")
+        
+        if not target:
+            return await m.edit(content=f"> {d['args-required']}")
+        
+        try:
+            await self.bot.db.unban_user(target.id)
+        except Exception as exc:
+            return await m.edit(content=f"> {d['error']} \n```py\n{exc}\n```")
+        else:
+            await m.edit(content=f"> {d['prohibit-unbanned'].replace('{TARGET}', {target})}")
     
     #rank
     @commands.is_owner()
