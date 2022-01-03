19505cf75e9c7e3ea7563aa8ca1a60ca97abe79b
midorichan <55083684+midorichaan@users.noreply.github.com>
2022-01-04 01:52:21 +0900

    Update mido_mod.py

--- cogs/mido_mod.py
+++ cogs/mido_mod.py
@@ -10,6 +10,10 @@ class mido_mod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    #check_hierarchy
+    def check_hierarchy(self, ctx, user, target):
+        return user == ctx.guild.owner or user.top_role > target.top_role
+
     #ban
     @commands.command(name="ban", usage="ban <Member/User> [Reson]")
     @commands.has_permissions(ban_members=True)
@@ -26,10 +30,37 @@ class mido_mod(commands.Cog):
         if target.id == ctx.author.id:
             return await m.edit(content=f"> {d['ban-cannot-self']}")
 
+        if not self.check_hierarchy(ctx, ctx.author, target):
+            return await m.edit(content="> {d['ban-cannot_ban']}")
+
         if not reason:
-            reason = d["ban-noreason"]
+            reason = d["punish-noreason"]
 
         await ctx.guild.ban(target, reason=reason)
+
+    #kick
+    @commands.command(name="kick", usage="kick <Member> [Reson]")
+    @commands.has_permissions(kick_members=True)
+    @commands.bot_has_permissions(kick_members=True)
+    @commands.guild_only()
+    async def kick(self, ctx, target: discord.Member=None, *, reason: str=None):
+        lang = await self.bot.langutil.get_user_lang(ctx.author.id)
+        d = await self.bot.langutil.get_lang(lang)
+        m = await utils.reply_or_send(ctx, content=f"> {d['loading']}")
+
+        if not target:
+            return await m.edit(content=f"> {d['member-required']}")
+
+        if target.id == ctx.author.id:
+            return await m.edit(content=f"> {d['kick-cannot-self']}")
+
+        if not self.check_hierarchy(ctx, ctx.author, target):
+            return await m.edit(content="> {d['kick-cannot_kick']}")
+
+        if not reason:
+            reason = d["punish-noreason"]
+
+        await ctx.guild.kick(target, reason=reason)
 
 def setup(bot):
     bot.add_cog(mido_mod(bot))
