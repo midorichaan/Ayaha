928803de85e67c7b52c955215af5a96938d35d80
midorichan <55083684+midorichaan@users.noreply.github.com>
2022-01-04 01:38:38 +0900

    Update mido_mod.py

--- cogs/mido_mod.py
+++ cogs/mido_mod.py
@@ -14,13 +14,14 @@ class mido_mod(commands.Cog):
     @commands.command(name="ban", usage="ban <Member/User> [Reson]")
     @commands.has_permissions(ban_members=True)
     @commands.bot_has_permissions(ban_members=True)
+    @commands.guild_only()
     async def ban(self, ctx, target: Union[discord.Member, discord.User, utils.FetchUserConverter]=None, *, reason: str=None):
         lang = await self.bot.langutil.get_user_lang(ctx.author.id)
         d = await self.bot.langutil.get_lang(lang)
         m = await utils.reply_or_send(ctx, content=f"> {d['loading']}")
 
         if not target:
-            return await m.edit(content=f"> {d['args-required']}")
+            return await m.edit(content=f"> {d['member-required']}")
 
         if target.id == ctx.author.id:
             return await m.edit(content=f"> {d['ban-cannot-self']}")
