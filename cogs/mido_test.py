39a7515524fd54886bb6787a480f6a622c62c1ca
midorichan <55083684+midorichaan@users.noreply.github.com>
2022-01-13 18:59:21 +0900

    Update mido_test.py

--- cogs/mido_test.py
+++ cogs/mido_test.py
@@ -1,10 +1,26 @@
 import discord
 from discord.ext import commands
 
+import time
+
+from discord_slash import SlashCommand, SlashContext, cog_ext
+
 class mido_test(commands.Cog):
+
+    slash = SlashCommand(bot, sync_commands=True)
 
     def __init__(self, bot):
         self.bot = bot
+
+    #test slash
+    @slash.slash(name="slash")
+    async def _slash(self, ctx: SlashContext):
+        mt = time.perf_counter()
+        m = await ctx.send(content="> 処理中...")
+
+        ws = round(self.bot.latency * 1000, 2)
+        ping = round(time.perf_counter() - mt, 3) * 1000
+        await m.edit(content=f"> Pong! \nPing: {ping}ms \nWebSocket: {ws}ms")
 
 def setup(bot):
     bot.add_cog(mido_test(bot))
