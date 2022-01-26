8957aa73194e3795249a86ca72f65f9991c6de41
midorichan <55083684+midorichaan@users.noreply.github.com>
2022-01-26 11:28:53 +0900

    [Patch] delete delay (move to mido_traffic_info)

--- cogs/mido_info.py
+++ cogs/mido_info.py
@@ -69,59 +69,6 @@ class mido_info(commands.Cog):
             "関西空港/和歌山方面": "関西空港・和歌山"
         }
 
-    #delay
-    @commands.command(name="delay", usage="delay <line>")
-    async def delay(self, ctx, line: str=None):
-        lang = await self.bot.langutil.get_user_lang(ctx.author.id)
-        d = await self.bot.langutil.get_lang(lang)
-        m = await utils.reply_or_send(ctx, content=f"> {d['loading']}")
-
-        if not line:
-            return await m.edit(content="> {d['args-required']}")
-
-        if not line.endswith("線"):
-            line = f"{line}線"
-
-        _ = line
-        e = discord.Embed(title=f"{_}の遅延情報", color=self.bot.color, timestamp=ctx.message.created_at)
-        line = self.jrwlines.get(line, None)
-        if not line:
-            return await m.edit(content="> 路線が見つからなかったよ！")
-
-        try:
-            ret = urllib.request.urlopen(f"https://www.train-guide.westjr.co.jp/api/v3/{line}.json")
-            ret_st = urllib.request.urlopen(f"https://www.train-guide.westjr.co.jp/api/v3/{line}_st.json")
-            ret = json.loads(ret.read().decode("utf-8"))
-            ret_st = json.loads(ret_st.read().decode("utf-8"))
-
-            p = None
-            data = {}
-
-            for s in ret_st["stations"]:
-                data[s["info"]["code"]] = s["info"]["name"]
-            for i in ret["trains"]:
-                if i["delayMinutes"] > 0:
-                    st = i["pos"].split("_")
-
-                    try:
-                        p = data[st[0]] + "辺り"
-                    except KeyError:
-                        p = "不明"
-
-                    type = self.jrwrapids.get(i['displayType'], i['displayType'])
-                    if "特急" in type:
-                        type = type + f" {i['nickname']}"
-                    e.add_field(
-                        name=f"{type} {self.jrwdesti.get(i['dest']['text'], i['dest']['text'])}行き", 
-                        value=f"約{i['delayMinutes']}分遅れ (走行位置: {p})"
-                    )
-
-            if not bool(e.fields):
-                e.add_field(name="列車遅延", value=d["none"])
-            return await m.edit(content=None, embed=e)
-        except Exception as exc:
-            return await m.edit(conetent=f"> {d['error']} \n```py\n{exc}\n```")
-
     #shadowban
     @commands.command(aliases=["sb"], usage="shadowban <twitter_id>")
     async def shadowban(self, ctx, twitter_id=None):
