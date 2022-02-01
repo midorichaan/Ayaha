6273421221aad74f88365e2f0519d94219db5724
midorichan <55083684+midorichaan@users.noreply.github.com>
2022-02-01 11:09:38 +0900

    [Add] 途中まで

--- cogs/mido_global.py
+++ cogs/mido_global.py
@@ -1,14 +1,23 @@
 import discord
 from discord.ext import commands
+from enum import Enum
 
 import aiohttp
 import re
+
+class DBType(Enum):
+    FETCHONE = 1
+    FETCHALL = 2
+    EXECUTE = 3
 
 class mido_global(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.session = bot.session or aiohttp.ClientSession()
+
+        if not hasattr(bot, "session"):
+            bot.session = aiohttp.ClientSession()
+        self.session = bot.session
 
         self.badges = {
             "staff": "🔒",
@@ -19,6 +28,11 @@ class mido_global(commands.Cog):
             "sgc_mods": "🔨"
         }
 
+        self.react = {
+            "exc": "❌",
+            "success": "✅"
+        }
+
         self.sgc = {
             "guild_id": 706905953320304772,
             "channel_id": 707158257818664991,
@@ -26,75 +40,55 @@ class mido_global(commands.Cog):
             "channel": "test"
         }
 
+        self.enabled = True
+
     ####events
     @commands.Cog.listener()
     async def on_webhooks_update(self, channel):
-        try:
-            db = await self.bot.db.fetchone(
-                "SELECT * FROM globalchat WHERE channel_id=%s",
-                (channel.id,)
-            )
-        except Exception as exc:
-            print(f"[Error] {exc}")
-            return
-        else:
-            if db:
-                me = channel.guild.get_member(self.bot.user.id)
-                cp = channel.permissions_for(me)
-                if cp.manage_webhooks:
-                    hook = discord.utils.get(
-                        await channel.webhooks(),
-                        name="ayaha-global"
+        db = await self.get_db(
+            DBType.FETCHONE, 
+            query=f"SELECT * FROM guilds WHERE channel_id={channel.id}"
+        )
+
+        if db and not isinstance(db, Exception):
+            me = channel.guild.get_member(self.bot.user.id)
+            cp = channel.permissions_for(me)
+            if cp.manage_webhooks:
+                hook = discord.utils.get(
+                    await channel.webhooks(),
+                    name="ayaha-global"
+                )
+                if not hook:
+                    await self.get_db(
+                        DBType.EXECUTE, 
+                        query=f"DELETE FROM globalchat WHERE channel_id={channel.id}"
                     )
-                    if not hook:
-                        try:
-                            await self.bot.db.execute(
-                                "DELETE FROM globalchat WHERE channel_id=%s",
-                                (channel.id,)
-                            )
-                        except Exception as exc:
-                            print(f"[Error] {exc}")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        try:
-            db = await self.bot.db.fetchall(
-                "SELECT * FROM globalchat WHERE guild_id=%s",
-                (guild.id,)
+        db = await self.get_db(
+            DBType.FETCHALL,
+            query=f"SELECT * FROM globalchat WHERE guild_id={guild.id}"
+        )
+
+        if db and not isinstance(db, Exception):
+            await self.get_db(
+                DBType.EXECUTE,
+                query=f"DELETE FROM globalchat WHERE guild_id={guild.id}"
             )
-        except Exception as exc:
-            print(f"[Error] {exc}")
-            return
-        else:
-            if db:
-                try:
-                    await self.bot.db.execute(
-                        "DELETE FROM globalchat WHERE guild_id=%s",
-                        (guild.id,)
-                    )
-                except Exception as exc:
-                    print(f"[Error] {exc}")
 
     @commands.Cog.listener()
     async def on_guild_channel_delete(self, channel):
-        try:
-            db = await self.bot.db.fetchone(
-                "SELECT * FROM globalchat WHERE channel_id=%s",
-                (channel.id,)
+        db = await self.get_db(
+            DBType.FETCHONE,
+            query=f"SELECT * FROM globalchat WHERE channel_id={channel.id}"
+        )
+
+        if db and not isintance(db, Exception):
+            await self.get_db(
+                DBType.EXECUTE,
+                query=f"DELETE FROM globalchat WHERE channel_id={channel.id}"
             )
-        except Exception as exc:
-            print(f"[Error] {exc}")
-            return
-        else:
-            if db:
-                try:
-                    await self.bot.db.execute(
-                        "DELETE FROM globalchat WHERE channel_id=%s",
-                        (channel.id,)
-                    )
-                except Exception as exc:
-                    print(f"[Error] {exc}")
-                    return
 
     ####globalchat utils
     def check_content(self, msg):
@@ -169,7 +163,60 @@ class mido_global(commands.Cog):
             )
         return (message.id, msg.id, msg.guild.id, msg.channel.id, msg.author.id, str(msg.content), channel)
 
-    
+    async def check_db(self):
+        try:
+            await self.bot.db.execute("SELECT 1")
+        except:
+            self.enabled = False
+        else:
+            self.enabled = True
+
+    async def get_db(self, type, *, query):
+        if type == "1":
+            try:
+                return await self.bot.db.fetchone(query)
+            except Exception as exc:
+                return exc   
+        elif type == "2":
+            try:
+                return await self.bot.db.fetchall(query)
+            except Exception as exc:
+                return exc
+        elif type == 3:
+            try:
+                return await self.bot.db.execute(query)
+            except Exception as exc:
+                return exc
+
+    #send global chat
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if msg.guild:
+            await self.check_db()
+
+            if msg.guild.id == self.sgc["guild_id"]:
+                if self.sgc["channel"] == "test":
+                    if msg.channel.id == self.sgc["test_channel_id"]:
+                        
+                elif self.sgc["channel"] == "main":
+                    pass
+            else:
+                try:
+                    db = await self.bot.db.fetchone(
+                        "SELECT * FROM globalchat WHERE channel_id=%s", 
+                        (msg.channel.id,)
+                    )
+                except Exception as exc:
+                    print(f"[Error] {exc}")
+                    return
+                else:
+                    if db:
+                        all = await self.bot.db.fetchall(
+                            "SELECT * FROM globalchat WHERE channel=%s",
+                            (db["channel"],)
+                        )
+
+                        
 
 def setup(bot):
     bot.add_cog(mido_global(bot))
