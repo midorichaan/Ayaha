8adc937ce7755b1466a83c673ff3c19464531d81
midorichan <55083684+midorichaan@users.noreply.github.com>
2022-02-05 00:51:40 +0900

    [Add] timeout

--- cogs/mido_mod.py
+++ cogs/mido_mod.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 from typing import Union
 
-from lib import utils
+from lib import utils, times
 
 class mido_mod(commands.Cog):
 
@@ -13,6 +13,32 @@ class mido_mod(commands.Cog):
     #check_hierarchy
     def check_hierarchy(self, ctx, user, target):
         return user == ctx.guild.owner or user.top_role > target.top_role
+
+    #timeout_member
+    async def timeout_member(self, guild_id: int, target_id: int, *, duration=None, reason: str=None):
+        payload = {}
+        headers = {
+            "Authorization": f"Bot {self.bot.http.token}",
+            "User-Agent": "DiscordBot ()",
+            "X-Audit-Log-Reason": str(reason)
+        }
+
+        if duration:
+            payload["communication_disabled_until"] = str(duration)
+        else:
+            payload["communication_disabled_until"] = None
+
+        async with self.bot.session.request(
+            "PATCH",
+            f"https://discord.com/api/v9/guilds/{guild_id}/members/{target_id}",
+            headers=headers,
+            json=payload
+        ) as request:
+            data = await discord.http.json_or_text(request)
+            if request.status != 200:
+                raise Exception(f"api returns: {data}")
+            else:
+                return data
 
     #check_db
     async def check_db(self):
@@ -76,6 +102,40 @@ class mido_mod(commands.Cog):
             reason = d["punish-noreason"]
 
         await ctx.guild.kick(target, reason=reason)
+
+    #timeout
+    @commands.command(name="timeout", description="メンバーをタイムアウトするよ！", usage="timeout <member> <duration> [reason]")
+    @commands.guild_only()
+    @commands.has_permissions(kick_members=True, ban_members=True)
+    @commands.bot_has_permissions(kick_members=True, ban_members=True)
+    async def _timeout(self, ctx, target: discord.Member=None, duration: times.ShortTime=None, *, reason: str=None):
+        m = await utils.reply_or_send(ctx, content="> 処理中...")
+
+        if not target:
+            return await m.edit(content="> メンバーを指定してね！")
+
+        try:
+            await self.timeout_member(ctx, target, duration=duration.dt, reason=reason)
+        except Exception as exc:
+            return await m.edit(content=f"> エラー \n```py\n{exc}\n```")
+        else:
+            return await m.edit(content=f"> {target}をタイムアウトしたよ！ ({duration})")
+
+    #untimeout
+    @commands.command(name="untimeout", description="メンバーのタイムアウトを解除するよ！", usage="untimeout <member> [reason]")
+    @commands.guild_only()
+    async def _untimeout(self, ctx, target: discord.Member=None, *, reason: str=None):
+        m = await utils.reply_or_send(ctx, content="> 処理中...")
+
+        if not target:
+            return await m.edit(content="> メンバーを指定してね！")
+
+        try:
+            await self.timeout_member(ctx.guild.id, target.id, duration=None, reason=reason)
+        except Exception as exc:
+            return await m.edit(content=f"> エラー \n```py\n{exc}\n```")
+        else:
+            return await m.edit(content=f"> {target}のタイムアウトを解除したよ！")
 
 def setup(bot):
     bot.add_cog(mido_mod(bot))
